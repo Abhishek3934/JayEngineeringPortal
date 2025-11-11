a67d5fcc0b313ba59820cadc2b0f71a42a5e4625
Abhishek3934 <gangurdeabhi87@gmail.com>
2025-11-12 03:52:35 +0530

    Configured pkg declaration

--- src/main/java/com/JayEngineeringPortal/config/WebConfig.java
+++ src/main/java/com/JayEngineeringPortal/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.JayEngineeringPortal.config;
+package com.JayEngineeringPortal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
